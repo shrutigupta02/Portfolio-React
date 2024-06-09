3ef34290fc33bf90d585184b6887dffd4a308fd3
shrutigupta02 <shrutiiguptaaaa@gmail.com>
2024-06-10 00:12:18 +0530

    Updated Contact.jsx

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -1,16 +1,42 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import MailIcon from '@mui/icons-material/Mail';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-export default function Contact(){
-    return(
+export default function Contact() {
+    return (
         <div>
-            <Header/>
-            <div className="h-screen">
-            <h3 className="text-yellow-500 text-6xl mb-4 font-bold hover:italic">
-                Reach out to me!
-            </h3>
+            <Header />
+            <div className="m-5 p-3 h-screen">
+                <h3 className="text-yellow-500 text-6xl mb-4 font-bold hover:italic">
+                    Reach out to me!
+                </h3>
+                <div className="mt-4 h-2/3 flex justify-center items-center ">
+                    <div className="bg-white shadow-lg m-4 rounded-lg p-2 transform hover:scale-105 transition-transform duration-300 flex flex-col justify-between" style={{ width: '300px' }}>
+                        <MailIcon style={{ fontSize: 40 }} className="text-yellow-800 mb-4"/>
+                        <h4 className="mb-2 text-yellow-800 text-2xl font-bold">Email</h4>
+                        <a href="mailto:shrutiguptaindore02@gmail.com" className="text-yellow-600 text-lg font-medium italic hover:text-blue-800 hover:underline mt-auto">
+                        shrutiguptaindore02@gmail.com
+                        </a>
+                    </div>
+                    <div className="bg-white shadow-lg m-4 rounded-lg p-2 transform hover:scale-105 transition-transform duration-300 flex flex-col justify-between" style={{ width: '300px' }}>
+                        <LinkedInIcon style={{ fontSize: 40 }} className="text-yellow-800 mb-4"/>
+                        <h4 className="mb-2 text-yellow-800 text-2xl font-bold">LinkedIn</h4>
+                        <a href="https://www.linkedin.com/in/shrutigupta2/" className="text-yellow-600 text-lg font-medium italic hover:text-blue-800 hover:underline mt-auto">
+                            Shruti Gupta
+                        </a>
+                    </div>
+                    <div className="bg-white shadow-lg m-4 rounded-lg p-4 transform hover:scale-105 transition-transform duration-300 flex flex-col justify-between" style={{ width: '300px' }}>
+                        <GitHubIcon style={{ fontSize: 40 }} className="text-yellow-800 mb-4"/>
+                        <h4 className="mb-2 text-yellow-800 text-2xl font-bold">GitHub</h4>
+                        <a href="https://github.com/shrutigupta02" className="text-yellow-600 text-lg font-medium italic hover:text-blue-800 hover:underline mt-auto">
+                            @shrutigupta02
+                        </a>
+                    </div>
+                </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -2,28 +2,25 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { projects } from "../utils/projects";
 
-export default function Projects(){
-    
-
-    return(
+export default function Projects() {
+    return (
         <div>
-            <Header/>
-            <div className="m-5 p-3 h-screen">
+            <Header />
+            <div className="m-5 p-3 h-full">
                 <h3 className="text-yellow-500 text-6xl font-bold hover:italic">My Projects:</h3>
-                <div className="mt-4">
-                    <ul>
-                        {projects.map((project)=>(
-                            <li className="my-3">
-                                <h4 className="mb-2 text-yellow-800 text-2xl font-bold hover:italic">{project.name}</h4>
-                                <p className="mb-2 text-md">{project.description}</p>
-                                <a href={project.link}
-                                className="mb-2 text-inherit text-lg font-medium italic hover:text-inherit hover:underline">{project.link}</a>
-                            </li>
-                        ))}
-                    </ul>
+                <div className="mt-4 flex">
+                    {projects.map((project) => (
+                        <div key={project.name} className="w-96 m-4 shadow-lg rounded-lg p-6 transform hover:scale-105 transition-transform duration-300">
+                            <h4 className="mb-2 text-yellow-800 text-2xl font-bold hover:italic">{project.name}</h4>
+                            <p className="mb-2 text-md">{project.description}</p>
+                            <a href={project.link} className="text-yellow-600 text-sm font-medium italic hover:text-blue-800 hover:underline">
+                                {project.link}
+                            </a>
+                        </div>
+                    ))}
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
-    )
+    );
 }
